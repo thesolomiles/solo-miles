ef07e465092b9b4d1476ab33a3f4feecd8ea19e2
Leonard <leonardgoal@gmail.com>
2026-08-25 11:23:56 +0800

    Save interaction zones (4) from ?zones

--- src/config/zones.data.ts
+++ src/config/zones.data.ts
@@ -17,4 +17,5 @@ export const INTERACT_ZONES: InteractZone[] = [
   { id: "ze6h68k", verb: "Enter cafe", minX: 11.6, maxX: 12.8, minZ: -0.7, maxZ: 1.4 },
   { id: "zkjs0ff", verb: "Use vending machine", minX: 5.8, maxX: 6.7, minZ: -2.1, maxZ: 0.5 },
   { id: "z9mqziw", minX: -9.7, maxX: -7, minZ: -1.4, maxZ: 2.8 },
+  { id: "zbydnja", verb: "Forest roads ahead", minX: -3.6, maxX: -1.7, minZ: -5.7, maxZ: -3.3 },
 ]
